8a3a919d692813425e78860a1fae10b355ab8ea1
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-21 15:08:50 +0300

    create config for db settings

---
+++ app/core/config.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Application-wide settings.
+    """
+    api_prefix: str = "/api/v1"
+    project_name: str = "Insurance Service"
+    version: str = "1.0.0"
+    debug: bool = False
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str = "postgresql"
+    postgres_port: int = 5432
+    db_echo: bool = False
+
+    @property
+    def db_url(self) -> str:
+        """
+        Dynamically generate the database URL.
+        """
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:"
+            f"{self.postgres_password}@{self.postgres_host}:"
+            f"{self.postgres_port}/{self.postgres_db}"
+        )
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
