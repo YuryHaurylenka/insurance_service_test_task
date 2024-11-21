44c3b0ea3428bb18ee39b1647a972ead60b8c0fa
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-21 23:20:35 +0300

    add setup_logging to main.py file

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from app.api.router import router
 from app.core.config import settings
+from app.core.logging import setup_logging
+
+setup_logging()
 
 app = FastAPI(
     title=settings.project_name,
