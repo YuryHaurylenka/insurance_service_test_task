d0fb8f2860e5f60ef5088ef55d56e4646e83d567
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-21 23:20:11 +0300

    add user_id to actionLog table

--- app/models/logs.py
+++ app/models/logs.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, JSON, String
+from sqlalchemy import DateTime, JSON, String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
@@ -10,10 +10,11 @@ class ActionLog(Base):
     """
     Model for storing logs of user actions in the system.
     """
+
     __tablename__ = "action_logs"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    user_id: Mapped[str | None] = mapped_column(String, nullable=True)
     action: Mapped[str] = mapped_column(String, nullable=False)
     payload: Mapped[dict] = mapped_column(JSON, nullable=False)
     timestamp: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    user_id: Mapped[int | None] = mapped_column(Integer, nullable=True)
