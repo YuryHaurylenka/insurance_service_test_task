9feb02d2d1d04156d58c81eda461ceb564642b68
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-22 03:36:33 +0300

    add _missing classmethod to validate uppercase or lowercase

--- app/models/tariff.py
+++ app/models/tariff.py
@@ -3,7 +3,9 @@ import enum
 from sqlalchemy import Date, Enum, Float
 from sqlalchemy.orm import Mapped, mapped_column
 
+from app.core.config import settings
 from app.models.base import Base
+from app.services.kafka import add_to_log_buffer
 
 
 class CargoType(enum.Enum):
@@ -11,6 +13,15 @@ class CargoType(enum.Enum):
     METAL = "Metal"
     WOOD = "Wood"
     OTHER = "Other"
+
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            value_upper = value.upper()
+            for member in cls:
+                if member.value == value_upper:
+                    return member
+        return None
 
 
 class Tariff(Base):
